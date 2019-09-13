34c3aa086c791fa1d60dd0842b593d7745366be3
dengqing <1247748612@qq.com>
2019-09-13 23:07:38 +0800

    恢复到最初始阶段

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import _ from 'lodash';
-import './index.css';
-import icon from './icon.jpg'
-import Data from './data.xml'
-import printMe from './print'
 
 
 function component() {
@@ -11,21 +7,8 @@ function component() {
   // Lodash, now imported by this script
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello')
-  const image = new Image()
-  image.src = icon;
 
-  element.appendChild(image);
-  console.log(Data);
   return element;
 }
 
-function addButton () {
-  const element = document.createElement('Button');
-  element.innerHTML= 'Click me and check the console!';
-  element.onclick = printMe;
-  return element;
-}
-
-// document.body.appendChild(component());
-document.body.appendChild(addButton())
+document.body.appendChild(component());
