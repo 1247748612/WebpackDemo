e7a6f2c14e1642cd4375ff1bf396e86ce98f8326
dengqing <1247748612@qq.com>
2019-09-14 00:30:40 +0800

    production-construct finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import _ from 'lodash';
+import { cube } from './main.js';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 
 function component() {
-  var element = document.createElement('div');
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  // Lodash, now imported by this script
+  var element = document.createElement('pre');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
 
   return element;
 }
